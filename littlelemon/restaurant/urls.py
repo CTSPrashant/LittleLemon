5646e586beb51b66b94412e236ecbbc2e008200c
CTSPrashant <911916@cognizant.com>
2025-12-15 18:14:16 +0530

    updated for adding a rest api freamwork

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin 
 from django.urls import path 
-from .views import sayHello, index
+from .views import sayHello, index, MenuItemsView, SingleMenuItemView
   
 urlpatterns = [ 
     # path('', sayHello, name='sayHello'), 
-    path('', index, name='index')
+    # path('', index, name='index'),
+    path('menu/', MenuItemsView.as_view(), name='menu-list'),
+    path('menu/<int:pk>', SingleMenuItemView.as_view(), name='menu-details'),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import Booking
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from .models import Menu
+from .serializers import MenuSerializer, BookinSerializer
 
 def sayHello(request):
  return HttpResponse('Hello World')
@@ -7,3 +12,18 @@ def sayHello(request):
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+
+# Create your views here. 
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+   queryset = Booking.objects.all()
+   serializer_class = BookinSerializer
